ffca29460bb267f47a2e2fbe4d60d48825cc227a
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2023-07-17 16:37:55 -0400

    Create Item.java

---
+++ Final Projects/Item.java
@@ -0,0 +1,19 @@
+public class Item {
+
+  private String name;
+  private double price;
+
+  public Item (String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public void display () {
+    System.out.println ("  - " + name + " for $" + price);
+  }
+
+  public double get_price () {
+    return price;
+  }
+  
+}
