5bd78189693e4bf1a6d68687af0441f7e8ae8f6a
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2023-09-22 18:43:52 -0400

    Create concentration.py

---
+++ concentration.py
@@ -0,0 +1,43 @@
+import random
+import time
+import os
+
+def print_board ():
+  line = ""
+  for i in range (size):
+    if (i + 1) % 5 == 0:
+      print (line)
+      line = ""
+    else:
+      line += board[i] + " "
+  print (line)
+
+print ("\nWelcome to Concentration! \n")
+
+size = int (input("How many total symbols will be on the board? "))
+
+symbols = ['!', '@', '#', '$', '%']
+board = []
+
+for _ in range (size):
+  index = random.randint (0, 4)
+  board.append (symbols[index])
+
+print ("\nYou have 10 seconds to memorize the following board: \n")
+print_board ()
+
+time.sleep (10)
+os.system ('clear')
+
+print ("\nResults: \n")
+exclamation_marks = int (input ("How many '!' did you see on the board? "))
+at_signs = int (input ("How many '@' did you see on the board? "))
+hashtags = int (input ("How many '#' did you see on the board? "))
+dollar_signs = int (input ("How many '$' did you see on the board? "))
+percentage_signs = int (input ("How many '%' did you see on the board? "))
+
+print ("\nAmount of ! =", board.count ("!"))
+print ("Amount of @ =", board.count ("@"))
+print ("Amount of # =", board.count ("#"))
+print ("Amount of $ =", board.count ("$"))
+print ("Amount of % =", board.count ("%"))
