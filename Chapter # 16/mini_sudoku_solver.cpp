6338f0fbe419436ef7f423dd7e82487f67f4ae84
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2023-10-01 12:32:43 -0400

    Update mini_sudoku_solver.cpp

--- Chapter # 16/mini_sudoku_solver.cpp
+++ Chapter # 16/mini_sudoku_solver.cpp
@@ -1,129 +1,49 @@
-#include <ctype.h>
 #include <iostream>
-#include <set>
-#include <string>
-#include <vector>
+#include <set>  
 
-std::vector<int> get_row(std::string line) {
-  std::vector<int> row;
-  for (int i = 0; i < line.length(); i++) {
-    if (isdigit(line[i])) {
-      row.push_back(line[i] - '0');
-    }
-  }
-  return row;
-}
-
-std::vector<std::vector<int>> get_board() {
-  std::vector<std::vector<int>> board;
-  std::cout << "\nEnter the 4x4 board row by row: \n";
-  for (int i = 1; i <= 4; i++) {
-    std::string line;
-    std::cout << "\nRow # " << i << ": ";
-    std::getline(std::cin, line);
-    board.push_back(get_row(line));
-  }
-  return board;
-}
-
-bool check_rows(std::vector<std::vector<int>> board) {
-  for (int i = 0; i < 4; i++) {
-    std::set<int> row_set;
-    for (int j = 0; j < 4; j++) {
-      int num = board[i][j];
-      if (num < 1 || num > 4 || row_set.count(num) > 0) {
-        return false;
-      }
-      row_set.insert(num);
-    }
-  }
-  return true;
-}
-
-bool check_columns(std::vector<std::vector<int>> board) {
-  for (int i = 0; i < 4; i++) {
-    std::set<int> column_set;
-    for (int j = 0; j < 4; j++) {
-      int num = board[j][i];
-      if (num < 1 || num > 4 || column_set.count(num) > 0) {
-        return false;
-      }
-      column_set.insert(num);
-    }
-  }
-  return true;
-}
-
-bool check_quadrant1(std::vector<std::vector<int>> board) {
-  for (int i = 0; i < 2; i++) {
-    std::set<int> quadrant_set;
-    for (int j = 0; j < 2; j++) {
-      int num = board[i][j];
-      if (num < 1 || num > 4 || quadrant_set.count(num) > 0) {
-        return false;
-      }
-      quadrant_set.insert(num);
-    }
-  }
-  return true;
-}
-
-bool check_quadrant2(std::vector<std::vector<int>> board) {
-  for (int i = 0; i < 2; i++) {
-    std::set<int> quadrant_set;
-    for (int j = 2; j < 4; j++) {
-      int num = board[i][j];
-      if (num < 1 || num > 4 || quadrant_set.count(num) > 0) {
-        return false;
-      }
-      quadrant_set.insert(num);
-    }
-  }
-  return true;
-}
-
-bool check_quadrant3(std::vector<std::vector<int>> board) {
-  for (int i = 2; i < 4; i++) {
-    std::set<int> quadrant_set;
-    for (int j = 0; j < 2; j++) {
-      int num = board[i][j];
-      if (num < 1 || num > 4 || quadrant_set.count(num) > 0) {
-        return false;
-      }
-      quadrant_set.insert(num);
-    }
-  }
-  return true;
-}
-
-bool check_quadrant4(std::vector<std::vector<int>> board) {
-  for (int i = 2; i < 4; i++) {
-    std::set<int> quadrant_set;
-    for (int j = 2; j < 4; j++) {
-      int num = board[i][j];
-      if (num < 1 || num > 4 || quadrant_set.count(num) > 0) {
-        return false;
-      }
-      quadrant_set.insert(num);
-    }
-  }
-  return true;
+bool valid (std::set <int> s) {
+  return s.size() == 4 && s.count (1) == 1 && s.count (2) == 1 && s.count (3) == 1 && s.count (4) == 1; 
 }
 
 int main() {
 
-  std::vector<std::vector<int>> board = get_board();
+  std::cout << "\nEnter the 4x4 board row by row:\n";
 
-  bool a = check_rows(board);
-  bool b = check_columns(board);
-  bool c = check_quadrant1(board);
-  bool d = check_quadrant2(board);
-  bool e = check_quadrant3(board);
-  bool f = check_quadrant4(board);
+  int r1c1, r1c2, r1c3, r1c4;
+  std::cout << "\nRow # 1: ";
+  std::cin >> r1c1 >> r1c2 >> r1c3 >> r1c4;
 
-  if (a && b && c && d && e && f) {
-    std::cout << "\nSolved correctly! \n";
+  int r2c1, r2c2, r2c3, r2c4;
+  std::cout << "\nRow # 2: ";
+  std::cin >> r2c1 >> r2c2 >> r2c3 >> r2c4;
+
+  int r3c1, r3c2, r3c3, r3c4;
+  std::cout << "\nRow # 3: ";
+  std::cin >> r3c1 >> r3c2 >> r3c3 >> r3c4;
+
+  int r4c1, r4c2, r4c3, r4c4;
+  std::cout << "\nRow # 4: ";
+  std::cin >> r4c1 >> r4c2 >> r4c3 >> r4c4;
+
+  std::set <int> row1 = {r1c1, r1c2, r1c3, r1c4};
+  std::set <int> row2 = {r2c1, r2c2, r2c3, r2c4};
+  std::set <int> row3 = {r3c1, r3c2, r3c3, r3c4};
+  std::set <int> row4 = {r4c1, r4c2, r4c3, r4c4};
+
+  std::set <int> column1 = {r1c1, r2c1, r3c1, r4c1};
+  std::set <int> column2 = {r1c2, r2c2, r3c2, r4c2};
+  std::set <int> column3 = {r1c3, r2c3, r3c3, r4c3};
+  std::set <int> column4 = {r1c4, r2c4, r3c4, r4c4};
+
+  std::set <int> quadrant1 = {r1c1, r1c2, r2c1, r2c2};
+  std::set <int> quadrant2 = {r1c3, r1c4, r2c3, r2c4};
+  std::set <int> quadrant3 = {r3c1, r3c2, r4c1, r4c2};
+  std::set <int> quadrant4 = {r3c3, r3c4, r4c3, r4c4};
+
+  if (valid (row1) && valid (row2) && valid (row3) && valid (row4) && valid (column1) && valid (column2) && valid (column3) && valid (column4) && valid (quadrant1) && valid (quadrant2) && valid (quadrant3) && valid (quadrant4)) {
+    std::cout << "\nSolved correctly!\n";
   } else {
-    std::cout << "\nSolved incorrectly. \n";
+    std::cout << "\nSolved incorrectly.\n";
   }
+  
 }
