aa5229c00a5b18eb526ec9f6a4eaf62d715993df
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2023-10-07 11:47:54 -0400

    Create J5.py

---
+++ 2016/J5.py
@@ -0,0 +1,15 @@
+question = int (input())
+n = int (input())
+dmojistan = [int (i) for i in input().split()]
+pegland = [int (i) for i in input().split()]
+
+dmojistan.sort()
+pegland.sort()
+
+if question == 2:
+  pegland = pegland[::-1]
+
+speed = 0
+for i in range (n):
+  speed += max (dmojistan[i], pegland[i])
+print (speed)
