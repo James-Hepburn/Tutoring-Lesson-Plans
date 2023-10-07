7a7f5060b1f52f26235b27d34635d11b1ff87bbb
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2023-10-07 11:37:04 -0400

    Create J5.py

---
+++ 2021/J5.py
@@ -0,0 +1,19 @@
+M = int (input())
+N = int (input())
+K = int (input())
+rows = [0] * M
+cols = [0] * N
+
+for i in range (K):
+  art = input().split()
+  if art [0] == 'R':
+    rows [int (art[1]) - 1] += 1
+  else:
+    cols [int (art[1]) - 1] += 1
+
+gold = 0
+for i in range (M):
+  for j in range (N):
+    if (rows [i] + cols [j]) % 2 == 1: # an odd number of instructions
+      gold += 1
+print(gold)
