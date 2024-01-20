1d22477892be5e18bb6ebb3e8d285c72e7e69621
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2024-01-20 13:27:14 -0500

    Update S1.py

--- 2020/S1.py
+++ 2020/S1.py
@@ -1,28 +1,21 @@
 # ccc senior s1 2020 - https://dmoj.ca/problem/ccc20s1
 
 n = int (input ())
-stats = {}
+stats = []
 
 for _ in range (n):
   time, pos = [int (i) for i in input ().split ()]
-  stats [time] = pos
+  stats.append ([time, pos])
 
-stats = dict (sorted (stats.items ()))
-
-times = []
-positions = []
-
-for i in stats:
-  times.append (i)
-  positions.append (stats [i])
+stats = sorted (stats, key = lambda x : x [0])
 
 max_speed = 0
 
 for i in range (n - 1):
-  cur_time = times [i]
-  next_time = times [i + 1]
-  cur_pos = positions [i]
-  next_pos = positions [i + 1]
+  cur_time = stats [i][0]
+  next_time = stats [i + 1][0]
+  cur_pos = stats [i][1]
+  next_pos = stats [i + 1][1]
   speed = abs (next_pos - cur_pos) / abs (next_time - cur_time)
   max_speed = max (max_speed, speed)
 
