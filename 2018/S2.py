7b0f01fa974dc179a6882be9e380909d905b4902
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2024-02-10 11:34:28 -0500

    Create S2.py

---
+++ 2018/S2.py
@@ -0,0 +1,36 @@
+# ccc senior s2 2018 - https://dmoj.ca/problem/ccc18s2
+
+def rotate (matrix, size):
+  rotated_matrix = []
+  for i in range (size):
+    rotated_matrix.append([0] * size)
+
+  for i in range (size):
+    for j in range (size):
+      rotated_matrix[j][size - 1 - i] = matrix[i][j]
+
+  return rotated_matrix
+
+def valid_grid (grid):
+  for i in grid:
+    for j in range (len (grid) - 1):
+      if i[j] >= i[j+1]:
+        return False
+  for i in range (len (grid[0])):
+    for j in range (len (grid) - 1):
+      if grid[j][i] >= grid[j+1][i]:
+        return False
+  return True
+
+N = int (input())
+grid = []
+
+for i in range (N):
+  grid.append ([int (i) for i in input().split()])
+  
+while not valid_grid (grid):
+  grid = rotate (grid, N)
+   
+for i in grid:
+  str_i = [str(j) for j in i]
+  print (" ".join (str_i))
