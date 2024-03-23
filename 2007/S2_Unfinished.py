0d4699d65b76e28a3903e2b7ebcf3758038129f7
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2024-03-23 12:56:47 -0400

    Update S2_Unfinished.py

--- 2007/S2_Unfinished.py
+++ 2007/S2_Unfinished.py
@@ -1,1 +1,25 @@
+# ccc senior s2 2007 - https://dmoj.ca/problem/ccc07s2
 
+n = int (input ())
+boxes = [[int (i) for i in input ().split ()] for _ in range (n)]
+m = int (input ())
+packages = [[int (i) for i in input ().split ()] for _ in range (m)]
+
+for i in packages:
+  sizes = [
+    [i [0], i [1], i [2]], 
+    [i [0], i [2], i [1]],
+    [i [1], i [0], i [2]],
+    [i [1], i [2], i [0]],
+    [i [2], i [0], i [1]],
+    [i [2], i [1], i [0]]
+  ]
+  volume = 100000
+  for j in sizes:
+    for k in boxes:
+      if j [0] <= k [0] and j [1] <= k [1] and j [2] <= k [2]:
+        volume = min (volume, k [0] * k [1] * k [2])
+  if volume == 100000:
+    print ("Item does not fit.")
+  else:
+    print (volume)
