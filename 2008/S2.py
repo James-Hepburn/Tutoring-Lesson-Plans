7c5bc8f1a7fcaf7e2c106b8d2485a446e7753e34
James-Hepburn <90709675+James-Hepburn@users.noreply.github.com>
2024-03-23 11:44:36 -0400

    Update and rename S2_Unfinished.py to S2.py

---
+++ 2008/S2.py
@@ -0,0 +1,16 @@
+# ccc senior s2 2008 - https://dmoj.ca/problem/ccc08s2
+
+def calculate_pennies (radius):
+  pennies = 0
+  for i in range (1, radius + 1):
+    for j in range (0, radius + 1):
+      point = (i * i) + (j * j)
+      if point <= radius * radius:
+        pennies += 1
+  return int ((pennies * 4) + 1) # split circle into 4 quadrants + center penny
+
+while True:
+  radius = int (input ())
+  if radius == 0:
+    break
+  print (calculate_pennies (radius))
